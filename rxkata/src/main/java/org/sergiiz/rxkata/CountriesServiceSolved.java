028e7da629813f0a2d58268ca7e531ece97d521c
mikshka <michael.korshunov@gmail.com>
2018-04-11 12:24:17 +0300

    test

--- rxkata/src/main/java/org/sergiiz/rxkata/CountriesServiceSolved.java
+++ rxkata/src/main/java/org/sergiiz/rxkata/CountriesServiceSolved.java
@@ -1,14 +1,12 @@
 package org.sergiiz.rxkata;
 
+import io.reactivex.Observable;
+import io.reactivex.Single;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
-import java.util.stream.Collectors;
-
-import io.reactivex.Observable;
-import io.reactivex.Observer;
-import io.reactivex.Single;
 
 class CountriesServiceSolved implements CountriesService {
 
@@ -34,7 +32,7 @@ class CountriesServiceSolved implements CountriesService {
 
     @Override
     public Observable<Country> listOnly3rdAnd4thCountry(List<Country> countries) {
-       return Observable.fromIterable(countries).skip(2).take(2);
+        return Observable.fromIterable(countries).skip(2).take(2);
     }
 
     @Override
@@ -48,33 +46,38 @@ class CountriesServiceSolved implements CountriesService {
 
     @Override
     public Observable<Country> listPopulationMoreThanOneMillion(List<Country> countries) {
-        return null; // put your solution here
+
+        return Observable.fromIterable(countries).filter(e -> e.getPopulation() > 1000000l);
     }
 
     @Override
     public Observable<Country> listPopulationMoreThanOneMillionWithTimeoutFallbackToEmpty(final FutureTask<List<Country>> countriesFromNetwork) {
+        //Observable.fromIterable(countriesFromNetwork.)
         return null; // put your solution here
     }
 
     @Override
     public Observable<String> getCurrencyUsdIfNotFound(String countryName, List<Country> countries) {
-        return null; // put your solution here
+        return Observable.fromIterable(countries).filter(e -> countryName.equals(e.getName())).map(e -> e.getCurrency()).defaultIfEmpty("USD");
+        //return null; // put your solution here
     }
 
     @Override
     public Observable<Long> sumPopulationOfCountries(List<Country> countries) {
-        return null; // put your solution here
+        return Observable.fromIterable(countries).map(e -> e.getPopulation()).reduce((x, y) -> x + y).toObservable();
     }
 
     @Override
     public Single<Map<String, Long>> mapCountriesToNamePopulation(List<Country> countries) {
-        return null; // put your solution here
+        Map<String, Long> map = new HashMap<>();
+       // Observable.fromIterable(countries).map(e -> Mapmap.put(e.getName(),e.getPopulation()));
+        return Single.fromCallable( () -> map);
     }
 
     @Override
     public Observable<Long> sumPopulationOfCountries(Observable<Country> countryObservable1,
                                                      Observable<Country> countryObservable2) {
-        return null; // put your solution here
+        return countryObservable1.mergeWith(countryObservable2).map(e -> e.getPopulation()).reduce((x, y) -> x + y).toObservable();
     }
 
     @Override
